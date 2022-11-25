cdcf9cbd6375304492015b6252a27f7f06bff9da
Fabio Rodrigues <frsilva2101@gmail.com>
2022-11-25 16:14:36 -0300

    Pagina home, inicio

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
-<h1>TESTE</h1>
+import React from 'react';
+
+
+
+
+export default function () {
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+
+  const toggle = () => setDropdownOpen(prevState => !prevState);
+
+  return (
+      <div>
+          <footer>
+              <p>©Copyright 2022 by Tiringa Group.  All rights reversed.</p>
+          </footer>
+      </div>
+  )
+}
